4788daa283d13152d196e3e78fbd65eb19b80375
rmn7591 <rashmim282@gmail.com>
2022-02-18 15:06:11 +0000

    Decision tree examples

---
+++ pythonBasics/data/convertinguserinput.py
@@ -0,0 +1,4 @@
+#convert user input
+inp=input('which floor ?')
+usf= int(inp)+ 1
+print('US floor', usf)
---
+++ pythonBasics/hello.py
@@ -0,0 +1,4 @@
+print("Hello world\n")
+print(5+8)
+# this has been used to run the above file python3 hello.py
+
