ea1e90f2659963238de92be08bd342457588f6a5
rmn7591 <rashmim282@gmail.com>
2022-03-02 12:17:14 +0000

    basic python practice

---
+++ pythonBasics/armstrong.py
@@ -0,0 +1,20 @@
+num=153
+sum=0
+order=len(str(num))
+copy_num=num
+while(num>0):
+    digit = num%10
+    sum += digit **order
+    num = num//10
+# at the end of the iteration num will be zero so we are saving in copy_num
+if(sum == copy_num):
+    print(copy_num, "It is a armstrong number")
+else:
+     print(copy_num,"It is not an armstrong number ")
+
+
+
+
+
+
+
