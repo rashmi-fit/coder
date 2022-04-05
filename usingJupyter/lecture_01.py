31e6fc56369d59cc34bebf45c078d86267cadca0
rashmi-fit <rashmim282@gmail.com>
2022-04-05 21:14:08 +0100

    [RM] importing .ipynb to .py file

---
+++ usingJupyter/lecture_01.py
@@ -0,0 +1,96 @@
+# %% [markdown]
+# <h1> <center> <font color="green"> Welcome to Python!! </font></center></h1>
+
+# %% [markdown]
+# <b> Output Statement <b>
+
+# %%
+print("Hello world!!")
+
+# %%
+print('hello world')
+
+# %%
+print("this is ", "first", "code")
+
+# %%
+print('2+3')
+
+# %%
+print('2+3 is', 2+3)
+
+# %%
+print("line1")
+print("line2")
+
+# %%
+print("this is \n next line")
+
+# %%
+print("line1")
+print("line2")
+
+# %%
+print("line1", end=" ")
+print("line2")
+
+# %%
+print("line1", end="$")
+print("line2")
+
+# %%
+'hello print'
+
+# %% [markdown]
+# <b> Input statements <b>
+
+# %%
+input("Enter the year:")
+
+# %%
+year = input("Enter the year")
+
+# %%
+print(year)
+
+# %%
+year
+
+# %%
+year = input("Enter the year : ")
+year = year + 10
+print("Year after ten years will be ", year)
+
+# %%
+year = input("Enter the year : ")
+
+
+# %%
+year
+
+# %%
+type(year)
+
+# %%
+year = int(input("Enter the year : "))
+year = year + 10
+print("Year after ten years will be ", year)
+
+# %%
+type(year)
+
+# %%
+print("*" * 100)
+
+# %%
+#this is a comment
+
+# %%
+#this is a comment
+a = 10
+print(a)
+
+# %%
+
+
+
--- usingJupyter/readme.md
+++ usingJupyter/readme.md
@@ -1,1 +1,2 @@
-here is the beginning of jupyter nootebook work
+Tips:
+If you want to open a .ipynb in Visual studio or any editor,import the .ipynb file and save as .py file and can be run in python terminal.
