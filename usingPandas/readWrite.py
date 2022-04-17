93017646dd8a8ace8f4a29fb2779bb45ae795959
rashmi-fit <rashmim282@gmail.com>
2022-04-17 16:23:39 +0100

    [RM]Restructuring the project

---
+++ usingPandas/readWrite.py
@@ -0,0 +1,37 @@
+# x= 0
+# while (x < 100):
+#     x+=2
+# print(x)
+
+# if -3
+# x= 0
+# a= 0
+# b = -5
+# if a > 0:
+#     if b < 0: 
+#         x=x + 5
+#     elif a > 5:
+#         x=x + 4
+#     else:
+#         x=x + 3
+# else:
+#     x=x+ 2
+# print(x)
+
+
+# i= 1
+# while True:
+#     if i%2 == 0:
+#         break
+#     print(i)
+#     i += 2
+# x = ['ab', 'cd']
+# for i in x:
+#     i.upper()
+# print(x)
+
+numbers = [10, 20]
+items = ["Chair", "Table"]
+for x in numbers:
+    for y in items:
+        print(x, y)
---
+++ usingPandas/x.py
@@ -0,0 +1,10 @@
+# numbers = [10, 20]
+# items = ["Chair", "Table"]
+# for x in numbers:
+#     for y in items:
+#         print(x, y)
+
+
+        # chr(ord('D'))
+
+        # print ('{0:.3}'.format(2.0 / 3))
